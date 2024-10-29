4f8091158099cf2d839e1d04e0c8a799659223ec
Shujauddin <shujauddin.gh@.com>
2024-10-29 19:18:43 -0400

    refactor: move photo to About page, update text content, and remove logos

--- components/HomeContent.jsx
+++ components/HomeContent.jsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import { motion } from "framer-motion";
 import Social from "@/components/Social";
-import Photo from "./Photo";
 
 const HomeContent = ({ data }) => {
   const name = data?.name || "Nom par défaut";
@@ -22,8 +21,8 @@ const HomeContent = ({ data }) => {
       className="h-full"
     >
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left order-2 xl:order-none">
+        <div className="flex flex-col xl:flex-row items-center justify-between  xl:pb-50">
+          <div className="text-center xl:text-left xl:ml-40">
             <h1 className="h1">
               <span className="text-[24px] xl:text-[32px]">{`Hello! I'm`}</span>
             </h1>
@@ -34,9 +33,10 @@ const HomeContent = ({ data }) => {
               <span className="text-xl">{title}</span>
             </div>
             <p className="max-w-[550px] mb-9 text-justify">
-              Welcome to my portfolio! I’m glad you’re here! While I’m currently
-              working on my projects, this space will soon showcase my creative
-              journey and future endeavors. Stay tuned for updates!
+              Welcome to my portfolio! I&apos;m glad you&apos;re here! While
+              I&apos;m currently working on my projects, this space will soon
+              showcase my creative journey and future endeavors. Stay tuned for
+              updates!
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <div className="flex items-center">
@@ -64,9 +64,6 @@ const HomeContent = ({ data }) => {
                 />
               </div>
             </div>
-          </div>
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo />
           </div>
         </div>
       </div>
