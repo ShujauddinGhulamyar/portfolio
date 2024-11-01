528420202ef7610fc7c2672145d866d02fed96ad
Shujauddin <shujauddin.gh@.com>
2024-11-01 15:04:30 -0400

    feat: added SEO metadata and ErrorBoundary component for improved site structure and error handling.

---
+++ app/metadata.js
@@ -0,0 +1,28 @@
+// app/metadata.js
+export const metadata = {
+  title: "Shuja Ghulamyar - Full Stack Developer",
+  description:
+    "Welcome to the portfolio of Shuja Ghulamyar, a full stack developer based in Montreal, showcasing my projects and skills in web development.",
+  openGraph: {
+    title: "Shuja Ghulamyar - Full Stack Developer",
+    description:
+      "Explore my portfolio highlighting my achievements in web development.",
+    url: "https://shuja-ghulamyar.netlify.app/",
+    siteName: "Shuja Ghulamyar's Portfolio",
+    images: [
+      {
+        url: "https://shuja-ghulamyar.netlify.app/assets/image.png",
+        height: 630,
+        alt: "Shuja Ghulamyar - Full Stack Developer",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Shuja Ghulamyar - Full Stack Developer",
+    description: "Discover my projects and skills in web development.",
+    images: ["https://shuja-ghulamyar.netlify.app/assets/image.png"],
+  },
+};
