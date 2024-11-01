caec845859274ff1c136bc7357a82caffcbbab92
Shujauddin <shujauddin.gh@.com>
2024-11-01 12:59:46 -0400

    refactor: Merged HomeContent into page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,70 @@
 // app/page.jsx
-import HomeContent from "@/components/HomeContent";
+"use client";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+import { motion } from "framer-motion";
+import Social from "@/components/Social";
 
-async function fetchData() {
-  // Simule une récupération de données
-  return {
-    name: "Shuja Ghulamyar",
-    title: "A Full Stack Developer in Montreal",
-    bio: "Welcome to my portfolio! I’m glad you’re here!",
-  };
-}
+const Home = () => {
+  const name = "Shuja Ghulamyar";
+  const title = "A Full Stack Developer in Montreal";
 
-const Home = async () => {
-  const data = await fetchData();
-
-  return <HomeContent data={data} />;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.2, duration: 0.4, ease: "easeIn" }}
+      className="h-full"
+    >
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pb-50">
+          <div className="text-center xl:text-left xl:ml-40">
+            <h1 className="h1">
+              <span className="text-[24px] xl:text-[32px]">{`Hello! I'm`}</span>
+            </h1>
+            <h1 className="h1 mb-4">
+              <span className="text-accent">{name}</span>
+            </h1>
+            <div className="mb-10">
+              <span className="text-xl">{title}</span>
+            </div>
+            <p className="max-w-[550px] mb-9 text-justify">
+              Welcome to my portfolio! I&apos;m glad you&apos;re here! While
+              I&apos;m currently working on my projects, this space will soon
+              showcase my creative journey and future endeavors. Stay tuned for
+              updates!
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <div className="flex items-center">
+                <a
+                  href="/assets/Shuja_Ghulamyar_CV.pdf"
+                  download
+                  className="uppercase flex items-center gap-2"
+                  aria-label="Télécharger le CV"
+                >
+                  <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    transition={{ type: "spring" }}
+                  >
+                    <Button variant="outline" size="lg">
+                      <span>Download CV</span>
+                      <FiDownload className="text-xl" />
+                    </Button>
+                  </motion.div>
+                </a>
+              </div>
+              <div className="flex gap-6">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Home;
